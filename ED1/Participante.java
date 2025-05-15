d4dd9178685401aa2c994471ea8f462fce1eaf48
alexxisleon <alelion001@gmail.com>
2025-05-14 21:02:49 -0300

    Renomeado pasta 'jogo' para 'ED1'

---
+++ ED1/Participante.java
@@ -0,0 +1,35 @@
+
+package jogo;
+
+public class Participante {
+    private String apelido;
+    private int totalTentativas;
+    private int score;
+
+   
+    public Participante(String apelido) {
+        this.apelido = apelido;
+        this.totalTentativas = 0;
+        this.score = 0;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public int getTotalTentativas() {
+        return totalTentativas;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void registrarTentativa() {
+        totalTentativas++;
+    }
+
+    public void somarPontos(int pontos) {
+        score += pontos;
+    }
+}
